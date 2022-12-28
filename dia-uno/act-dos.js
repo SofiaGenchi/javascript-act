fb5b8f4a00d53dd35c35c79b6e3ecd820de9c01a
Sofia Genchi <sofigen16@gmail.com>
2022-12-28 10:58:55 -0300

    segundo dia de actividades

--- dia-uno/act-dos.js
+++ dia-uno/act-dos.js
@@ -1,5 +1,103 @@
 // Act. 1 Yo no Fui
 // Solicitar al usiario un nombre. Si el mismo es igual a otro nombre almacenado en una variable, realizar una salida por alerta con el mensaje "Fui yo". Caso contrario, la salida sera "Yo no fui".
 
-let nombre = "Sofia";
+// let nombre = "sofia";
+// let solicitarNombre = prompt("Decime tu nombre")
 
+// if (solicitarNombre === nombre){
+//     alert("Fui yo");
+// }else{
+//     alert("Yo no fui")
+// }
+
+
+// Act 2 Presionar Y
+// let tecla = prompt("Precione una tecla")
+
+// if (tecla === "y" || tecla === "Y"){
+//     alert("Correcto")
+// }else{
+//     alert("Error")
+// }
+
+
+//Act. 3 Escoger personaje
+// let numero = Number(prompt("Elegir un numero entre 1 y 4"))
+
+// if (numero >= 0 && numero < 5){
+//     if (numero === 1){
+//         alert("Elegiste a Homero")
+//     } else if (numero === 2){
+//         alert("Elegiste a Marge")
+//     } else if (numero === 3){
+//         alert("Elegiste a Bart")
+//     } else {
+//         alert("Elegiste a Lisa")
+//     }
+// } else{
+//     alert("El numero no corresponde")
+// }
+
+
+// Act 4 Presupuesto
+// let numero = Number(prompt("Se solicita un numero"))
+
+// if (numero >= 0 && numero <= 3000){
+//     if (numero >= 0 && numero <= 1000){
+//         alert("Presupuesto bajo")
+//     } else if (numero >= 1001 && numero < 3000){
+//         alert("Presupuesto medio")
+//     } else {
+//         alert("Presupusto alto")
+//     }
+// }
+
+// Act. 5 Vacio
+// let productoUno = prompt("Primer producto de almacen:")
+// let productoDos = prompt("Segundo producto de almacen:")
+// let productoTres = prompt("Tercer producto de almacen:")
+// let productoCuatro = prompt("Cuarto producto de almacen:")
+
+// if (productoUno != "" && productoDos != "" && productoTres != "" && productoCuatro != ""){
+//     alert("Tus productos son: " + productoUno + ", " + productoDos + ", " + productoTres + " y " + productoCuatro)
+// }else {
+//     alert("Error: Es necesario cargar todos los productos.")
+// }
+
+
+// Js. 3 ----------------------
+// Act. 3 El pizarron
+// let numero = Number(prompt("Numero:"))
+// let texto = String(prompt("Texto"))
+
+// for (let stop = 0; stop < numero; stop++){
+//     alert(texto)
+// }
+
+
+// Act. 2 El cuadrado ordinario
+// let lados = prompt("Ingresar cantidad de lados")
+// emplear este valor para determinar la cantidad de repeticiones, y efectuar una salida por alerta con el mensaje "lado" en cada repeticion. Pero si se alcanza un numero de iteraciones mayor que 4, cancelar el ciclo.
+
+// for (let i = 0; i < lados; i++){
+//     if (i > 3) {
+//         break
+//     }
+//     alert("lado")
+// }
+
+// Act. 3 Registro de alumnos
+// let alumnos = ""
+
+// for (let i = 0; i < 10; i++){
+//     alumnos += prompt("Ingresar nombre de alumno") + "\n";
+// }
+// alert(alumnos)
+
+// Act 4. El innombrable
+let nombre = ""
+let i = 0
+let innombrable = "voldemort"
+do {
+    nombre += prompt("Ingresa nombre") + "\n"
+} while (nombre === innombrable)
